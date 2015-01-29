20141e4c750b9a6e473a0bf3f18ad4e5664075c6
David Pedersen <david.pdrsn@gmail.com>
2015-01-29 16:21:31 +0100

    Ignore compiled files

--- Pow.hs
+++ Pow.hs
@@ -1,17 +1,39 @@
 module Main where
 
-import Parse
+import Data.List
 import Interpreter
+import Parse
+import System.Directory
 import System.Environment
 import Text.ParserCombinators.Parsec (parseFromFile)
+
+doc :: [String]
+doc = [ "Usage: pow <filename>"
+      , ""
+      , "  <filename> should end in .pow"
+      ]
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [filename] -> do
+    [filename] ->
+      if ".pow" `isSuffixOf` filename
+      then runFile filename
+      else showDoc
+    _ -> showDoc
+
+showDoc :: IO ()
+showDoc = mapM_ putStrLn doc
+
+runFile :: String -> IO ()
+runFile filename = do
+  fileExists <- doesFileExist filename
+  case fileExists of
+    True -> do
       ast <- parseFromFile program filename
       case ast of
         Right ast' -> interpret ast'
         Left e -> putStrLn $ show e
-    _ -> putStrLn "Usage: pow <filename>"
+    _ -> putStrLn $ (show filename) ++ " was not found"
+
